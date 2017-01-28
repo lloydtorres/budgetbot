b00a0105a6e43a29af82812e62250b87266368ec
lloydtorres <lloyd.torres95@gmail.com>
2017-01-28 15:46:11 -0500

    Parse int from assistant data

--- app.js
+++ app.js
@@ -24,13 +24,10 @@ app.post('/', function (req, res) {
       { recepient: "University of Waterloo Tuition Bill", cost: 8000.00 },
       { recepient: "Waterloo North Hydro", cost: 120.00 }
     ];
-    console.log("STATUS: I'm actually being initialized!");
-    console.log("CASHMONEY: " + assistant.data.cashMoney);
-    console.log("BILLS: " + assistant.data.bills);
   }
 
   function checkBalance (assistant) {
-    assistant.ask('<speak>Your account balance is <say-as interpret-as="cardinal">' + assistant.data.cashMoney + '</say-as> dollars.</speak>');
+    assistant.ask('<speak>Your account balance is <say-as interpret-as="cardinal">' + parseInt(assistant.data.cashMoney) + '</say-as> dollars.</speak>');
   }
 
   let actionMap = new Map();
